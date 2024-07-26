13a4689aa54b9d530bff1586b373b63bcc806e61
wschoi-loca <choiwonseok@lottecard.co.kr>
2024-07-26 13:40:50 +0900

    a

--- gtm/attributes_key.js
+++ gtm/attributes_key.js
@@ -1,17 +1,48 @@
+var keyMapping = {
+  // data-gtm-page - data-gtm-body
+  gtm_page_title: "{{page title}}",   
+  gtm_screen_id: "{{page id}}", 
+  gtm_frame: "SPA", // "SPA" 고정값
+  gtm_card_name: "{{카드 신청 단계 - 카드명}}",
+  gtm_card_code: "{{카드 신청 단계 - 카드코드}}",
+  gtm_card_new_or_exist: "{{카드 신청 단계 = 신규신청 구분}}",
+  gtm_card_type: "{{카드 신청 단계 - 완편, 중편, 간편}}",
+  gtm_fin_prod_name: "{{대출명 - 단기카드대출_일반, 단기카드대출_카드대금}}",
+  gtm_fin_amount: "{{대출 - 신청 금액}}",
+  gtm_revol_rate: "{{리볼빙 - 리볼빙 결제 비율}}",
+  gtm_revol_term: "{{리볼빙 - 신청 기간}}",
+  gtm_prod_funnel_name: "{{신청단계}}",
+  
+  // data-gtm-section - data-gtm-body
+  gtm_section: "{{섹션 값}}",
 
-// 모든 요소를 가져온 후 'data-ep' 속성이 있는지 필터링
-var elements = Array.from(document.getElementsByTagName('*')).filter(function(element) {
-  return Array.from(element.attributes).some(function(attr) {
-    return attr.name.startsWith('data-ga');
-  });
-});
-
-// 필터링된 요소들에 클릭 이벤트 리스너 추가
-elements.forEach(function(element) {
-  element.addEventListener('click', function(ev) {
-    window.dataLayer.push({
-        ga_att: 
-    })
-  });
-});
-
+  //data-gtm-click & data-gtm-visibility - data-gtm-body 
+  gtm_event: "{{이벤트명 - cts / popup }}",
+  gtm_text: "{{클릭 텍스트}}",
+  gtm_category_depth1: "{{클릭요소 상위 텍스트1}}",
+  gtm_category_depth2: "{{클릭요소 상위 텍스트2}}",
+  gtm_category_depth3: "{{클릭요소 상위 텍스트3}}",
+  gtm_frame: "{{SPA}}", // "SPA" 고정값
+  gtm_search_keyword: "{{검색 완료 페이지에서 인풋 키워드}}",
+  gtm_search_type: "{{검색 타입}}",
+  gtm_search_result: "{{검색 결과 메뉴 및 결과 갯수}}",
+  gtm_search_result_click: "{{검색 결과 메뉴 및 결과 갯수}}",
+  gtm_card_name: "{{카드 신청 단계 - 카드명}}",
+  gtm_card_code: "{{카드 신청 단계 - 카드코드}}",
+  gtm_card_new_or_exist: "{{카드 신청 단계 = 신규신청 구분}}",
+  gtm_card_type: "{{카드 신청 단계 - 완편, 중편, 간편}}",
+  gtm_fin_prod_name: "{{대출명 - 단기카드대출_일반, 단기카드대출_카드대금}}",
+  gtm_fin_amount: "{{대출 - 신청 금액}}",
+  gtm_revol_rate: "{{리볼빙 - 리볼빙 결제 비율}}",
+  gtm_revol_term: "{{리볼빙 - 신청 기간}}",
+  gtm_prod_funnel_name: "{{신청단계}}",
+  gtm_apply_metric_exit: "{{카드 신청 이탈}}", // 값은 integer 1 로 고정
+  gtm_popup_metric_click: "{{팝업 클릭}}", // 값은 integer 1 로 고정
+  gtm_popup_metric_exit: "{{팝업 닫기}}", // 값은 integer 1 로 고정
+  gtm_cts_name: "{{LADM 컨텐츠명 - 부담없이 환승에서 만나요}}",
+  gtm_gtm_cts_id: "{{LADM 컨텐츠ID - LTS0000063451}}",
+  gtm_cts_sub_id: "{{LADM 서브컨텐츠ID - 0000063451ABC}}",
+  gtm_vertical_index: "{{컨텐츠 세로 인덱스 - 1 , 2, 3 ...}}",
+  gtm_horizontal_index: "{{컨텐츠 가로 인덱스 - 1 , 2, 3 ...}}",
+  gtm_extra_index: "{{컨텐츠 인덱스 (여분용)}}"
+};
